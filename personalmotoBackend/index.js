094deb948d699efe6082c60931fc4c6265262c75
Ujjval Porwal <ujjvalporwal76@gmail.com>
2023-11-03 00:52:17 +0530

    intial

--- personalmotoBackend/index.js
+++ personalmotoBackend/index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://www.personalmoto.pl/",
   credentials: true, //access-control-allow-credentials:true
   // optionSuccessStatus: 200,
   // "Access-Control-Allow-Origin": "*"
