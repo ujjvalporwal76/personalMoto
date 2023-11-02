5ccdd171456b82d221e1e89ddd2da3c4950c87e6
Ujjval Porwal <ujjvalporwal76@gmail.com>
2023-11-03 01:43:03 +0530

    nav

--- personalmoto/src/components/NavBar/Navbar.jsx
+++ personalmoto/src/components/NavBar/Navbar.jsx
@@ -146,77 +146,71 @@ function Navbar() {
                 Watched
               </a>
             </li>
-            {isLogin ? ( // Render these options if the user is logged in
-              <>
-                <li className="nav-item">
-                  <a className="nav-link" href="/logout">
-                    <BiLogOut />
-                    Logout
-                  </a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    id="navbarDropdown"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    <BiSolidUser /> My account
-                  </a>
-                  <ul
-                    className="dropdown-menu"
-                    aria-labelledby="navbarDropdown"
-                  >
-                    <li className="dropdown-menu-item">
-                      <a className="dropdown-item" href="/myaccount-statistics">
-                        Statistics
-                      </a>
-                    </li>
-                    <li className="dropdown-menu-item">
-                      <a
-                        className="dropdown-item"
-                        href="/myaccount-advertisements"
-                      >
-                        Advertisements
-                      </a>
-                    </li>
-                    <li className="dropdown-menu-item">
-                      <a
-                        className="dropdown-item"
-                        href="/myaccount-news/selling"
-                      >
-                        The News
-                      </a>
-                    </li>
-                    <li className="dropdown-menu-item">
-                      <a
-                        className="dropdown-item"
-                        href="/myaccount-payments/wallet"
-                      >
-                        Payments
-                      </a>
-                    </li>
-                    <li className="dropdown-menu-item">
-                      <a
-                        className="dropdown-item"
-                        href="/myaccount-settings/settings"
-                      >
-                        Settings
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-              </>
-            ) : (
-              // Render this option if the user is not logged in
+            {/* {isLogin ? ( // Render these options if the user is logged in */}
+            <>
               <li className="nav-item">
-                <a className="nav-link" href="/login">
-                  <BiSolidUser /> Log in | Register
+                <a className="nav-link" href="/logout">
+                  <BiLogOut />
+                  Logout
                 </a>
               </li>
-            )}
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <BiSolidUser /> My account
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li className="dropdown-menu-item">
+                    <a className="dropdown-item" href="/myaccount-statistics">
+                      Statistics
+                    </a>
+                  </li>
+                  <li className="dropdown-menu-item">
+                    <a
+                      className="dropdown-item"
+                      href="/myaccount-advertisements"
+                    >
+                      Advertisements
+                    </a>
+                  </li>
+                  <li className="dropdown-menu-item">
+                    <a className="dropdown-item" href="/myaccount-news/selling">
+                      The News
+                    </a>
+                  </li>
+                  <li className="dropdown-menu-item">
+                    <a
+                      className="dropdown-item"
+                      href="/myaccount-payments/wallet"
+                    >
+                      Payments
+                    </a>
+                  </li>
+                  <li className="dropdown-menu-item">
+                    <a
+                      className="dropdown-item"
+                      href="/myaccount-settings/settings"
+                    >
+                      Settings
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </>
+            {/* ) : ( */}
+            // Render this option if the user is not logged in
+            <li className="nav-item">
+              <a className="nav-link" href="/login">
+                <BiSolidUser /> Log in | Register
+              </a>
+            </li>
+            {/* )} */}
             <li className="nav-item">
               <button
                 type="button"
